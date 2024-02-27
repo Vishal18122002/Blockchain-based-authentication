1d1d1871663152b1b2d95b2f3524694a3b3e2f27
Vishal.G <100464577+Vishal18122002@users.noreply.github.com>
2024-02-27 15:03:02 +0530

    script.js

--- index/script.js
+++ index/script.js
@@ -1,11 +1,12 @@
-(function(){document.getElementById('login-form').addEventListener('submit', function(event) {
+(function(){
+    "use script";
+    document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     if (username === 'admin' && password === 'password') {
-        alert('Login successful');
-    } else {
-        document.getElementById('error-message').textContent = 'Invalid username or password';
-    }
-});
+        alert('Login successful');} 
+    else {
+        document.getElementById('error-message').textContent = 'Invalid username or password';}
+    });
 }());
